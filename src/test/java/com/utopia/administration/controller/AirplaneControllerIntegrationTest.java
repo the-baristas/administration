b9b9f984fbd6f1134b56169b04a77a4c4e768378
Charles Kim <charles.kim@smoothstack.com>
2021-04-04 22:52:22 -0400

    BAR-22 Complete integration tests

--- src/test/java/com/utopia/administration/controller/AirplaneControllerIntegrationTest.java
+++ src/test/java/com/utopia/administration/controller/AirplaneControllerIntegrationTest.java
@@ -1,52 +1,67 @@
 package com.utopia.administration.controller;
 
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.util.Arrays;
-import java.util.List;
-
+import com.utopia.administration.dao.AirplaneDao;
 import com.utopia.administration.entity.Airplane;
-import com.utopia.administration.service.AirplaneService;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-@TestPropertySource(locations = "classpath:application-integrationtest.properties")
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+// @TestPropertySource(locations =
+// "classpath:application-integrationtest.properties")
 public class AirplaneControllerIntegrationTest {
     @Autowired
-    private MockMvc mockMvc;
+    private WebTestClient webClient;
 
-    @MockBean
-    private AirplaneService airplaneService;
+    @Autowired
+    private AirplaneDao airplaneDao;
 
     @Test
-    public void findAirplanes_Status200() throws Exception {
+    public void findAllAirplanes_AirplanesList() {
+        Airplane airplane = new Airplane();
+        airplane.setFirstClassSeatsMax(0L);
+        airplane.setBusinessClassSeatsMax(0L);
+        airplane.setEconomyClassSeatsMax(0L);
+        airplaneDao.save(airplane);
+
+        webClient.get().uri("/airplanes").accept(MediaType.APPLICATION_JSON)
+                .exchange().expectStatus().isOk().expectHeader()
+                .contentType(MediaType.APPLICATION_JSON)
+                .expectBodyList(Airplane.class).hasSize(1);
+    }
+
+    @Test
+    public void findAirplaneById_ValidId_AirplaneFound() {
         Airplane airplane = new Airplane();
         airplane.setId(1L);
-        List<Airplane> allAirplanes = Arrays.asList(airplane);
-        when(airplaneService.findAllAirplanes()).thenReturn(allAirplanes);
+        airplane.setFirstClassSeatsMax(0L);
+        airplane.setBusinessClassSeatsMax(0L);
+        airplane.setEconomyClassSeatsMax(0L);
+        airplaneDao.save(airplane);
 
-        mockMvc.perform(get("/airplanes")).andDo(print())
-                .andExpect(status().isOk());
+        webClient.get().uri("/airplanes/{id}", airplane.getId())
+                .accept(MediaType.APPLICATION_JSON).exchange().expectStatus()
+                .isOk().expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody(Airplane.class).isEqualTo(airplane);
+    }
 
-        // client.get().uri("/airplanes")
-        //         .accept(MediaType.APPLICATION_JSON)
-        //         .exchange().expectStatus().isOk()
-        //         .expectHeader().contentType(MediaType.APPLICATION_JSON);
-        
-        // client.get().uri("/airplanes")
-        //         .exchange()
-        //         .expectStatus().isOk()
-        //         .expectBodyList(Airplane.class).hasSize(1).contains(airplane);
+    @Test
+    public void createAirplane_Airplane_AirplaneFound() throws Exception {
+        Airplane airplane = new Airplane();
+        airplane.setId(1L);
+        airplane.setFirstClassSeatsMax(1L);
+        airplane.setBusinessClassSeatsMax(1L);
+        airplane.setEconomyClassSeatsMax(1L);
+        airplaneDao.save(airplane);
+
+        webClient.post().uri("/airplanes")
+                .contentType(MediaType.APPLICATION_JSON).bodyValue(airplane)
+                .exchange().expectStatus().isCreated().expectHeader()
+                .contentType(MediaType.APPLICATION_JSON)
+                .expectBody(Airplane.class).isEqualTo(airplane);
     }
 }

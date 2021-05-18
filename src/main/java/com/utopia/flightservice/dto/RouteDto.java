3b2fcd19afb31f184dc6763cbb30b188390ba098
gabriellenoel <gabby@gabriellealexa.com>
2021-05-18 14:29:05 -0400

    fixed merge conflicts

--- src/main/java/com/utopia/flightservice/dto/RouteDto.java
+++ src/main/java/com/utopia/flightservice/dto/RouteDto.java
@@ -1,13 +1,9 @@
 package com.utopia.flightservice.dto;
-
-<<<<<<< HEAD
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
-=======
->>>>>>> 556ac07824d9ce7db5f9b680d49fbc57742bcf5d
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.PositiveOrZero;
